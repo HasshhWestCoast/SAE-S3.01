9fce15767e3eac5538319291cb52209fb81c6c3f
Tzeur <kou.anas07@gmail.com>
2024-12-24 18:39:04 +0100

    'editadd'

--- src/Vue/Insertion/FenAjoutLogement.java
+++ src/Vue/Insertion/FenAjoutLogement.java
@@ -20,11 +20,9 @@ import javax.swing.DefaultComboBoxModel;
 public class FenAjoutLogement extends JInternalFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JTextField textFieldAdresse;
-	private JTextField textFieldCodePostale;
-	private JTextField textFieldVille;
-	private JTextField textFieldPeriodeDeConstruction;
-	private JTextField textFieldIdImmeuble;
+	private JTextField textFieldAcquisition;
+	private JTextField textFieldIdLogement;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -49,39 +47,25 @@ public class FenAjoutLogement extends JInternalFrame {
 		getContentPane().setBackground(new Color(255, 255, 255));
 		getContentPane().setLayout(null);
 		
-		JLabel lblAdresse = new JLabel("Adresse");
-		lblAdresse.setBounds(80, 130, 45, 13);
-		getContentPane().add(lblAdresse);
+		JLabel lblDateAcquisition = new JLabel("Date d'acquisition");
+		lblDateAcquisition.setBounds(80, 130, 96, 13);
+		getContentPane().add(lblDateAcquisition);
 		
-		textFieldAdresse = new JTextField();
-		textFieldAdresse.setBounds(80, 146, 223, 19);
-		getContentPane().add(textFieldAdresse);
-		textFieldAdresse.setColumns(10);
+		textFieldAcquisition = new JTextField();
+		textFieldAcquisition.setBounds(80, 146, 223, 19);
+		getContentPane().add(textFieldAcquisition);
+		textFieldAcquisition.setColumns(10);
 		
-		JLabel lblCodePostale = new JLabel("Code Postale");
-		lblCodePostale.setBounds(80, 186, 66, 13);
-		getContentPane().add(lblCodePostale);
-		
-		textFieldCodePostale = new JTextField();
-		textFieldCodePostale.setBounds(80, 201, 223, 19);
-		getContentPane().add(textFieldCodePostale);
-		textFieldCodePostale.setColumns(10);
-		
-		JLabel lblVille = new JLabel("Ville");
-		lblVille.setBounds(80, 243, 45, 13);
-		getContentPane().add(lblVille);
-		
-		textFieldVille = new JTextField();
-		textFieldVille.setBounds(80, 262, 223, 19);
-		getContentPane().add(textFieldVille);
-		textFieldVille.setColumns(10);
+		JLabel lbNombreDePiece = new JLabel("Nombre de pieces");
+		lbNombreDePiece.setBounds(80, 302, 112, 13);
+		getContentPane().add(lbNombreDePiece);
 		
 		JLabel lblTypeDebien = new JLabel("Type de bien");
-		lblTypeDebien.setBounds(80, 304, 66, 13);
+		lblTypeDebien.setBounds(80, 185, 66, 13);
 		getContentPane().add(lblTypeDebien);
 
 		JComboBox comboBoxTypeDeBien = new JComboBox();
-		comboBoxTypeDeBien.setBounds(80, 320, 223, 21);
+		comboBoxTypeDeBien.setBounds(80, 205, 223, 21);
 		comboBoxTypeDeBien.setForeground(new Color(0, 0, 0));
 		comboBoxTypeDeBien.setFont(new Font("Sylfaen", Font.PLAIN, 12));
 		comboBoxTypeDeBien.setBackground(new Color(31, 153, 88));
@@ -89,30 +73,21 @@ public class FenAjoutLogement extends JInternalFrame {
 		comboBoxTypeDeBien.setToolTipText("ID Immeuble");
 		getContentPane().add(comboBoxTypeDeBien);
 		
-		JLabel lblPeriodeDeConstruction = new JLabel("Période de construction");
-		lblPeriodeDeConstruction.setBounds(80, 362, 131, 13);
-		getContentPane().add(lblPeriodeDeConstruction);
+		JLabel lblIdLogement = new JLabel("ID Logement");
+		lblIdLogement.setBounds(80, 68, 96, 13);
+		getContentPane().add(lblIdLogement);
 		
-		textFieldPeriodeDeConstruction = new JTextField();
-		textFieldPeriodeDeConstruction.setBounds(80, 377, 96, 19);
-		getContentPane().add(textFieldPeriodeDeConstruction);
-		textFieldPeriodeDeConstruction.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("ID Immeuble");
-		lblNewLabel.setBounds(80, 68, 96, 13);
-		getContentPane().add(lblNewLabel);
-		
-		textFieldIdImmeuble = new JTextField();
-		textFieldIdImmeuble.setBounds(80, 84, 223, 19);
-		getContentPane().add(textFieldIdImmeuble);
-		textFieldIdImmeuble.setColumns(10);
+		textFieldIdLogement = new JTextField();
+		textFieldIdLogement.setBounds(80, 84, 223, 19);
+		getContentPane().add(textFieldIdLogement);
+		textFieldIdLogement.setColumns(10);
 		
 		RoundedButton btnAnnuler = new RoundedButton("Annuler", 20);
-		btnAnnuler.setBounds(77, 455, 85, 21);
+		btnAnnuler.setBounds(80, 413, 85, 21);
 		getContentPane().add(btnAnnuler);
 		
 		RoundedButton btnAjouter = new RoundedButton("Ajouter", 20);
-		btnAjouter.setBounds(218, 455, 85, 21);
+		btnAjouter.setBounds(213, 413, 85, 21);
 		getContentPane().add(btnAjouter);
 		
 		JLabel lblNewLabel_1 = new JLabel("Ajout Logement");
@@ -126,7 +101,28 @@ public class FenAjoutLogement extends JInternalFrame {
 		separator.setBackground(new Color(31, 151, 83));
 		separator.setBounds(93, 45, 188, 2);
 		getContentPane().add(separator);
-		setBounds(100, 100, 423, 568);
+		
+		JSpinner spinnerNbPiece = new JSpinner();
+		spinnerNbPiece.setBounds(80, 320, 49, 20);
+		getContentPane().add(spinnerNbPiece);
+		
+		JLabel lblNumEtage = new JLabel("N° etage");
+		lblNumEtage.setBounds(198, 302, 71, 13);
+		getContentPane().add(lblNumEtage);
+		
+		JSpinner spinnerNumEtage = new JSpinner();
+		spinnerNumEtage.setBounds(198, 320, 49, 20);
+		getContentPane().add(spinnerNumEtage);
+		
+		JLabel lblSurfaceHabitable = new JLabel("Surface habitable");
+		lblSurfaceHabitable.setBounds(80, 243, 90, 13);
+		getContentPane().add(lblSurfaceHabitable);
+		
+		textField = new JTextField();
+		textField.setBounds(80, 261, 223, 19);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		setBounds(100, 100, 423, 529);
 
 	}
 }
