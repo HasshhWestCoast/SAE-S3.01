3aa41e67025caaf8da56f714a7bb8c9e4209ad04
Tzeur <kou.anas07@gmail.com>
2025-01-11 14:32:34 +0100

    'OUOUOU'

--- src/Vue/Insertion/FenAjoutLogement.java
+++ src/Vue/Insertion/FenAjoutLogement.java
@@ -19,10 +19,13 @@ import javax.swing.DefaultComboBoxModel;
 public class FenAjoutLogement extends JInternalFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JTextField textFieldAcquisition;
+	private JTextField textFieldDateAcquisition;
 	private JTextField textFieldIdLogement;
-	private JTextField textField;
+	private JTextField textFieldSurfaceHabitable;
 	private GestionFenAjoutLogement gestionFenAjoutLogement;
+	private JComboBox comboBoxTypeDeLogement;
+	private JSpinner spinnerNbPiece;
+	private JSpinner spinnerNumEtage;
 
 	/**
 	 * Create the frame.
@@ -37,10 +40,10 @@ public class FenAjoutLogement extends JInternalFrame {
 		lblDateAcquisition.setBounds(80, 130, 96, 13);
 		getContentPane().add(lblDateAcquisition);
 		
-		textFieldAcquisition = new JTextField();
-		textFieldAcquisition.setBounds(80, 146, 223, 19);
-		getContentPane().add(textFieldAcquisition);
-		textFieldAcquisition.setColumns(10);
+		textFieldDateAcquisition = new JTextField();
+		textFieldDateAcquisition.setBounds(80, 146, 223, 19);
+		getContentPane().add(textFieldDateAcquisition);
+		textFieldDateAcquisition.setColumns(10);
 		
 		JLabel lbNombreDePiece = new JLabel("Nombre de pieces");
 		lbNombreDePiece.setBounds(80, 302, 112, 13);
@@ -50,14 +53,14 @@ public class FenAjoutLogement extends JInternalFrame {
 		lblTypeDebien.setBounds(80, 185, 66, 13);
 		getContentPane().add(lblTypeDebien);
 
-		JComboBox comboBoxTypeDeBien = new JComboBox();
-		comboBoxTypeDeBien.setBounds(80, 205, 223, 21);
-		comboBoxTypeDeBien.setForeground(new Color(0, 0, 0));
-		comboBoxTypeDeBien.setFont(new Font("Sylfaen", Font.PLAIN, 12));
-		comboBoxTypeDeBien.setBackground(new Color(31, 153, 88));
-		comboBoxTypeDeBien.setModel(new DefaultComboBoxModel(new String[] {"Appartement", "Maison"}));
-		comboBoxTypeDeBien.setToolTipText("ID Immeuble");
-		getContentPane().add(comboBoxTypeDeBien);
+		comboBoxTypeDeLogement = new JComboBox();
+		comboBoxTypeDeLogement.setBounds(80, 205, 223, 21);
+		comboBoxTypeDeLogement.setForeground(new Color(0, 0, 0));
+		comboBoxTypeDeLogement.setFont(new Font("Sylfaen", Font.PLAIN, 12));
+		comboBoxTypeDeLogement.setBackground(new Color(31, 153, 88));
+		comboBoxTypeDeLogement.setModel(new DefaultComboBoxModel(new String[] {"Appartement", "Maison"}));
+		comboBoxTypeDeLogement.setToolTipText("ID Immeuble");
+		getContentPane().add(comboBoxTypeDeLogement);
 		
 		JLabel lblIdLogement = new JLabel("ID Logement");
 		lblIdLogement.setBounds(80, 68, 96, 13);
@@ -92,7 +95,7 @@ public class FenAjoutLogement extends JInternalFrame {
 		separator.setBounds(93, 45, 188, 2);
 		getContentPane().add(separator);
 		
-		JSpinner spinnerNbPiece = new JSpinner();
+		spinnerNbPiece = new JSpinner();
 		spinnerNbPiece.setBounds(80, 320, 49, 20);
 		getContentPane().add(spinnerNbPiece);
 		
@@ -100,7 +103,7 @@ public class FenAjoutLogement extends JInternalFrame {
 		lblNumEtage.setBounds(198, 302, 71, 13);
 		getContentPane().add(lblNumEtage);
 		
-		JSpinner spinnerNumEtage = new JSpinner();
+		spinnerNumEtage = new JSpinner();
 		spinnerNumEtage.setBounds(198, 320, 49, 20);
 		getContentPane().add(spinnerNumEtage);
 		
@@ -108,10 +111,10 @@ public class FenAjoutLogement extends JInternalFrame {
 		lblSurfaceHabitable.setBounds(80, 243, 90, 13);
 		getContentPane().add(lblSurfaceHabitable);
 		
-		textField = new JTextField();
-		textField.setBounds(80, 261, 223, 19);
-		getContentPane().add(textField);
-		textField.setColumns(10);
+		textFieldSurfaceHabitable = new JTextField();
+		textFieldSurfaceHabitable.setBounds(80, 261, 223, 19);
+		getContentPane().add(textFieldSurfaceHabitable);
+		textFieldSurfaceHabitable.setColumns(10);
 		
 		RoundedButton btnAjouterCompteur = new RoundedButton("Ajouter un compteur", 20);
 		btnAjouterCompteur.addActionListener(gestionFenAjoutLogement);
@@ -120,4 +123,29 @@ public class FenAjoutLogement extends JInternalFrame {
 		setBounds(300, 100, 423, 507);
 
 	}
+	
+	public String getDateAcquisition() {
+	    return textFieldDateAcquisition.getText();
+	}
+
+	public String getIdLogement() {
+	    return textFieldIdLogement.getText();
+	}
+
+	public String getSurfaceHabitable() {
+	    return textFieldSurfaceHabitable.getText();
+	}
+
+	public String getComboBoxTypeDeLogement() {
+	    return comboBoxTypeDeLogement.getSelectedItem().toString();
+	}
+
+	public String getNbPiece() {
+	    return spinnerNbPiece.getValue().toString();
+	}
+
+	public String getNumEtage() {
+	    return spinnerNumEtage.getValue().toString();
+	}
+
 }
